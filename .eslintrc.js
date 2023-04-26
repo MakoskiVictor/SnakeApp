8cf068c7f1825b9520d677a45b7513185b313ed8
MakoskiVictor <makoski.ed@gmail.com>
2023-04-26 11:36:49 -0300

    ignorePatterns

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  overrides: [
+    { files: ['*.ts', '*.tsx']}
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react', 'react-hooks', '@typescript-eslint'
+  ],
+  "ignorePatterns": ["*.js"],
+  rules: {
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
