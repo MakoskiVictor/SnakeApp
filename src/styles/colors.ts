c433d0c1d7cedbd22729c6c60b99587c4a1f5248
MakoskiVictor <makoski.ed@gmail.com>
2023-04-27 20:05:22 -0300

    enum of colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,13 @@
+interface ColorsProp {
+  primary: string
+  secondary: string
+  tertiary: string
+  background: string
+}
+
+export const Colors: ColorsProp = {
+  primary: '#365314',
+  secondary: '#84cc16',
+  tertiary: '#eab308',
+  background: '#ecfccb'
+} as const
