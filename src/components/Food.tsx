4ed4986f4577d21a1e7996e207e59f012ff00f96
MakoskiVictor <makoski.ed@gmail.com>
2023-05-08 12:15:53 -0300

    added randomForuit function & styles

--- src/components/Food.tsx
+++ src/components/Food.tsx
@@ -1,20 +1,22 @@
 import { StyleSheet, Text } from 'react-native'
 import { type Coordenate } from '../types/types'
 
-/* function getRandomFruitEmoji (): string {
+function getRandomFruitEmoji (): string {
   const fruitEmojis = ['🍎', '🍊', '🍋', '🍇', '🍉', '🍓', '🍑', '🍍']
   const randomIndex = Math.floor(Math.random() * fruitEmojis.length)
   return fruitEmojis[randomIndex]
-} */
+}
 
-export default function Food ({ x, y }: Coordenate): JSX.Element {
-  return <Text style={[{ top: y * 10, left: x * 10 }, styles.food]}> 🍎 </Text>
+const fruit = getRandomFruitEmoji()
+
+export function Food ({ x, y }: Coordenate): JSX.Element {
+  return <Text style={[{ top: y * 10, left: x * 10 }, styles.food]}> { fruit } </Text>
 }
 
 const styles = StyleSheet.create({
   food: {
-    width: 20,
-    height: 20,
+    width: 30,
+    height: 30,
     borderRadius: 7,
     position: 'absolute'
   }
