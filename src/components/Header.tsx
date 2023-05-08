db963151d414a00d3861cf82cb8ab8de9e95e892
MakoskiVictor <makoski.ed@gmail.com>
2023-05-08 11:55:32 -0300

    Added styles to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { View, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Ionicons, FontAwesome } from '@expo/vector-icons'
 import { Colors } from '../styles/colors'
 
 interface HeaderProps {
@@ -11,13 +11,35 @@ interface HeaderProps {
 
 export function Header ({ reloadGame, pauseGame, children, isPaused }: HeaderProps): JSX.Element {
   return (
-        <View style={styles.container}></View>
+    <View style={styles.container}>
+    <TouchableOpacity onPress={reloadGame}>
+      <Ionicons name="reload-circle" size={35} color={Colors.primary} />
+    </TouchableOpacity>
+
+    <TouchableOpacity onPress={pauseGame}>
+      <FontAwesome
+        name={isPaused ? 'play-circle' : 'pause-circle'}
+        size={35}
+        color={Colors.primary}
+      />
+    </TouchableOpacity>
+    {children}
+  </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 0.05,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderColor: Colors.primary,
     borderWidth: 12,
-    borderColor: Colors.primary
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    borderBottomWidth: 0,
+    padding: 15,
+    backgroundColor: Colors.background
   }
 })
